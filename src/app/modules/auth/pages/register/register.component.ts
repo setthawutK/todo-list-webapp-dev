f6a358931e2699d4a823258cbb5e092466ba1bbd
NatthawutOngkhachuen <natthawutongkhachuen@gmail.com>
2025-08-24 22:12:27 +0700

    feat: Create login & register pages

---
+++ src/app/modules/auth/pages/register/register.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  imports: [],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class RegisterComponent {
+
+}
